6f3ab9b4dbe06c965bed904a38d4fe31f9afb873
Quinton Mincy <73136662+Quinton-Mincy@users.noreply.github.com>
2023-03-13 21:41:30 -0400

    Update Color_Controller.v

--- Diffusion-Limited-Aggregation/Color_Controller.v
+++ Diffusion-Limited-Aggregation/Color_Controller.v
@@ -6,33 +6,19 @@ module Color_Controller (
 
 always @(posedge iCLK) begin
   if (iSW[17]) begin
-    // RED
-		//oLEDR[17] <=1;
+    	 //red
 		oColor_Walker <= 16'hF000;
   end
-  /*
-  else if (iSW[16]) begin
-    //Pink
-		oColor_Walker <= 16'hF0FF;
-  end
-  else if (iSW[15]) begin
-    // yellow
-		oColor_Walker <= 16'hFF0F;
-  end
-  else if (iSW[14]) begin
-	 //pale yellow
-		oColor_Walker <= 16'hFFAF;
-  end
-  */
-  else if (iSW[13]) begin
+else if (iSW[16]) begin
 	 //coral
 		oColor_Walker <= 16'hFAAA;
   end
-  else if (iSW[12]) begin
-	 //grey
-		oColor_Walker <= 16'hAAAA;
+else if (iSW[15]) begin
+	 //yellow
+		oColor_Walker <= 16'hFF0F;
   end
   else begin 
+	  //white
 		oColor_Walker <= 16'hFFFF;
   end
 end
